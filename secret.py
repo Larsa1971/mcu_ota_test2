b162e75a13e7c0cfabfd97222a05d65fc56c11c5
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-10-20 22:58:39 +0200

    Update secret.py

--- secret.py
+++ secret.py
@@ -8,4 +8,6 @@ USER = "dittgithubnamn"
 REPO = "dittrepo"
 BRANCH = "main"
 FILE_PATH = "app_main.py"   # filen vi vill hålla uppdaterad
-CHECK_INTERVAL = 43200 # 1800 = 30 min 43 200 = 12 tim tiden i sekunder för verifiering om ny version kommit
+CHECK_INTERVAL_OTA = 86400 # 1800 = 30 min 43 200 = 12 tim tiden i sekunder för verifiering om ny version kommit
+CHECK_INTERVAL_WIFI = 60 # hur många sekunder mellan wifi koll
+TIME_SYNC_REPEAT = 24 # Efter hur länge tiden i timmar ska tiden syncks igen
