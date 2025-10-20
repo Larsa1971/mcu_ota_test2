cb4389354dca83ff1255c542b5e661fcb80c707a
Lars Lindqvist <37276018+Larsa1971@users.noreply.github.com>
2025-10-20 19:20:18 +0200

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,11 @@
+# WiFi
+WIFI_SSID = "DIN_SSID"
+WIFI_PASSWORD = "DITT_LÖSENORD"
+
+# GitHub
+GITHUB_TOKEN = "ghp_xxxDIN_TOKENxxx"
+USER = "dittgithubnamn"
+REPO = "dittrepo"
+BRANCH = "main"
+FILE_PATH = "app_main.py"   # filen vi vill hålla uppdaterad
+CHECK_INTERVAL = 43200 # 1800 = 30 min 43 200 = 12 tim tiden i sekunder för verifiering om ny version kommit
